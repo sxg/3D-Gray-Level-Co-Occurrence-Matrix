0409cbd47488f10406bcd7ba51ab24816d418ca1
Satyam Ghodasara <sghodas@gmail.com>
2017-09-22 18:34:13 -0400

    Fixed bugs in trimVolume.m

--- trimVolume.m
+++ trimVolume.m
@@ -1,9 +1,9 @@
 function [ trimmedVol ] = trimVolume( Vol )
 %trimVolume Removes all zero planes in the volume.
 
-flatZeroPlane = zeros(size(Vol, 1), size(Vol, 2));
-uprightZeroPlane = zeros(size(Vol, 3), size(Vol, 2));
-sideZeroPlane = zeros(size(Vol, 3), size(Vol, 1));
+flatZeroPlane = zeros(size(Vol, 1), size(Vol, 2), 1);
+uprightZeroPlane = zeros(1, size(Vol, 2), size(Vol, 3));
+sideZeroPlane = zeros(size(Vol, 1), 1, size(Vol, 3));
 
 firstNonZeroX = 1;
 lastNonZeroX = size(Vol, 2);
@@ -12,42 +12,48 @@ lastNonZeroY = size(Vol, 1);
 firstNonZeroZ = 1;
 lastNonZeroZ = size(Vol, 3);
 for x = 1:size(Vol, 2)
-    if Vol(:, x, :) ~= sideZeroPlane
+    if ~isequal(Vol(:, x, :), sideZeroPlane)
         firstNonZeroX = x;
+        break;
     end
 end
 
 for x = size(Vol, 2):-1:1
-    if Vol(:, x, :) ~= sideZeroPlane
+    if ~isequal(Vol(:, x, :), sideZeroPlane)
         lastNonZeroX = x;
+        break;
     end
 end
 
 for y = 1:size(Vol, 1)
-    if Vol(y, :, :) ~= uprightZeroPlane
+    if ~isequal(Vol(y, :, :), uprightZeroPlane)
         firstNonZeroY = y;
+        break;
     end
 end
 
 for y = size(Vol, 1):-1:1
-    if Vol(y, :, :) ~= uprightZeroPlane
-        firstNonZeroY = y;
+    if ~isequal(Vol(y, :, :), uprightZeroPlane)
+        lastNonZeroY = y;
+        break;
     end
 end
 
 for z = 1:size(Vol, 3)
-    if Vol(:, :, z) ~= flatZeroPlane
+    if ~isequal(Vol(:, :, z), flatZeroPlane)
         firstNonZeroZ = z;
+        break;
     end
 end
 
 for z = size(Vol, 3):-1:1
-    if Vol(:, :, z) ~= flatZeroPlane
-        firstNonZeroZ = z;
+    if ~isequal(Vol(:, :, z), flatZeroPlane)
+        lastNonZeroZ = z;
+        break;
     end
 end
 
-trimmedVol = Vol(firstNonZeroY:lastNonZeroY, firstNonZeroX:lastNonZeroX, firstNonZeroZ, lastNonZeroZ);
+trimmedVol = Vol(firstNonZeroY:lastNonZeroY, firstNonZeroX:lastNonZeroX, firstNonZeroZ:lastNonZeroZ);
 
 end
 
