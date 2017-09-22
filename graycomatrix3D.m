1a932d24a47c9068c8dabdd3a52898f246832515
Satyam Ghodasara <sghodas@gmail.com>
2017-09-22 10:42:23 -0400

    Added input handling

--- graycomatrix3D.m
+++ graycomatrix3D.m
@@ -1,7 +1,23 @@
-function [ glcms, SI ] = graycomatrix3D( V )
+function [ glcms, SV ] = graycomatrix3D( V, varargin )
 %graycomatrix3D Create gray-level co-occurrence matrix for 3D images.
 %   Detailed explanation goes here
 
+% Add inputs
+p = inputParser;
+% A volume matrix is required
+addRequired(p, 'V');
+% NumLevels is the number of gray levels in the image
+addParameter(p, 'NumLevels', 1);
+% offset is the set of offsets to evaluate
+addParameter(p, 'offset', [0, 1, 0]);
+% Symmetric considers the ordering of values
+addParameter(p, 'Symmetric', false);
+
+% Get inputs
+parse(p, V, varargin{:});
+numLevels = p.Results.NumLevels;
+offsetArray = p.Results.offset;
+symmetric = p.Results.symmetric;
 
 end
 
